9b09266f6d7949b5842435327b447e6c76357885
Rayane Ribeiro <46855211+Rayane420@users.noreply.github.com>
2020-03-08 19:38:36 -0300

    Alteração classe principal
    Alteração na lógica da classe principal

--- src/jogoparouimpar/JogoParOuImpar.java
+++ src/jogoparouimpar/JogoParOuImpar.java
@@ -1,22 +1,3 @@
-/*
-Dois amigos, Alice e Bob, estão jogando um jogo muito simples, em que um deles grita ou "par" ou "ímpar" e o outro imediatamente
-responde ao contrário, respectivamente "ímpar" ou "par". Em seguida, ambos exibem ao mesmo tempo uma mão cada um,
-em que alguns dedos estão estendidos e outros dobrados. Então eles contam o número total de dedos estendidos. 
-Se a soma for par, quem gritou "par" ganha. Se a soma for ímpar, quem gritou "ímpar" ganha. 
-Por exemplo, suponhamos que a Alice gritou "par" e o Bob respondeu "ímpar". 
-Em seguida, Alice não deixou nenhum dos seus dedos estendidos, ao passo que Bob deixou três dedos estendidos.
-A soma então é três, que é ímpar, portanto Bob ganhou. Seu programa deve determinar quem ganhou, 
-tendo a informação de quem gritou par e o número de dedos estendidos de cada um.
-
-Entrada
-A entrada contém três linhas, cada uma com um número inteiro, P, D_1 e D_2, nesta ordem. Se P = 0 então Alice gritou "par", ao passo que se P=1 então Bob gritou "par". Os números D_1 e D_2 indicam, respectivamente, o número de dedos estendidos da Alice e do Bob.
-Saída
-Seu programa deverá imprimir uma única linha, contendo um único número inteiro, que deve ser 0 se Alice foi a ganhadora, ou 1 se Bob foi o ganhador.
-Restrições
-P = 0 ou P = 1
-0 ≤ D_1 ≤ 5
-0 ≤ D_2 ≤ 5
- */
 package jogoParOuImpar;
 
 import java.util.Scanner;
@@ -36,9 +17,9 @@ public class JogoParOuImpar {
         
         Scanner sc = new Scanner(System.in); //Criando o objeto para o Scanner
         
-        System.out.println("Alice, Informe se quer par ou impar: \n Escolha 0 para par e 1 para impar: "); //Solicitando que Alice Informe se quer par ou Impar
-        p = sc.nextInt(); //Lendo o dado informado por Alice
-        if(p < 0 && p > 1){
+        System.out.println("Informe quem será par: \n Escolha 0 para Alice e 1 para Bob: "); //Solicitando que Alice Informe se quer par ou Impar
+        p = sc.nextInt(); //Lendo o dado informado para a opção par
+        if(p < 0 || p > 1){
             System.out.println("opção informada está incorreta, informe novamente uma opção, entre 0 e 1");
             System.out.println("Alice, Informe se quer par ou impar: \n Escolha 0 para par e 1 para impar: "); //Solicitando que Alice Informe se quer par ou Impar
             p = sc.nextInt(); //Lendo o dado informado por Alice
@@ -66,9 +47,9 @@ public class JogoParOuImpar {
         if(p==1){ //Caso a opção informada anteriormente seja 1 Bob gritou par, ao passo que Alice é impar
             
             if(resto == 0){
-                System.out.println("0");
-            }else{
                 System.out.println("1");
+            }else{
+                System.out.println("0");
             }
         } 
         
